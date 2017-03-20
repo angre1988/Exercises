17de6d8388cbb2d4c5d8131e172f86ccfc15ac0b
Slava <semeniy.vyacheslav@gmail.com>
2017-03-20 10:10:01 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Exercise1 x = new Exercise1();
+
+        System.out.println("t = " + x.t);
+        System.out.println("y = " + x.y);
+
+    }
+}
