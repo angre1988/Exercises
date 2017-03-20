5f0bb0759e477c94c8a3bfa2386ba481f4b33fb3
Slava <semeniy.vyacheslav@gmail.com>
2017-03-20 14:26:24 +0200

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Exercise1 x = new Exercise1();
+        System.out.println("First exercise:");
+        Exercise1 test = new Exercise1();
+        System.out.println("t = " + test.t);
+        System.out.println("y = " + test.y);
 
-        System.out.println("t = " + x.t);
-        System.out.println("y = " + x.y);
+        System.out.println("Second exercise:");
+
+        Exercise2 test1 = new Exercise2();
+        System.out.println(test1.s);
+
+        System.out.println("Next exercise:");
+
 
     }
 }
